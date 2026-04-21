2790cbf5e418a0760ec2e8a01db4fcae07838df3
Prabhleen Kaur <prabhkang140@gmail.com>
2026-04-21 11:39:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,39 +22,3 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Connect DB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-// Connecting routes to server
-const authRoutes = require("./routes/authRoutes");
-
-app.use("/api/auth", authRoutes);
-
-// Test protected route
-const authMiddleware = require("./middleware/authMiddleware");
-
-app.get("/api/protected", authMiddleware, (req, res) => {
-  res.json({ msg: "Protected route accessed", user: req.user });
-});
-
-// Connect Routes
-const profileRoutes = require("./routes/profileRoutes");
-
-app.use("/api/profile", profileRoutes);
-
-
-const appointmentRoutes = require("./routes/appointmentRoutes");
-
-app.use("/api/appointments", appointmentRoutes);
-
-
-const medicalRoutes = require("./routes/medicalRoutes");
-
-app.use("/api/medical", medicalRoutes);
